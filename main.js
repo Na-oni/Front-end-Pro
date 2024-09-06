159d9e6e5824fc28aa3607dc55f85ec33cc9e649
Na-oni <miagoriacheva@gmail.com>
2024-09-06 12:17:30 +0300

    push

---
+++ main.js
@@ -0,0 +1,5 @@
+let first_string = prompt("Пожалуйста, введите первую строку");
+let second_string = prompt("Пожалуйста, введите вторую строку");
+let third_string = prompt("Пожалуйста, введите третью строку");
+
+alert(`Первая строка: ${first_string}\nВторая строка:  ${second_string}\nТретья строка: ${third_string}`);
