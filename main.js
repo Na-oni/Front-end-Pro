d3ad185e677865a90b73832b97bfd0f5d3da85ea
Na-oni <miagoriacheva@gmail.com>
2024-09-09 11:21:33 +0300

    push

---
+++ main.js
@@ -0,0 +1,20 @@
+let number = prompt("Введите число");
+
+alert(`[0] = ${number[0]}, [1] = ${number[1]}, [2] = ${number[2]}`);
+
+if (number[0] === number[1] && number[1] === number[2]) {
+    alert('В числе все цифры одинаковые');
+} else if (number[0] === number[1] || number[1] === number[2] || number[1] === number[2]) {
+    alert('В числе есть одинаковые цифры');
+}
+
+// Или так, но с извращениями
+for(let i = 0; i < number.length; i++) {
+    for (let j = i + 1; j < number.length; j++) {
+        console.log(`[${i}] = ${number[i]} И [${j}] = ${number[j]}`);
+        if(i !== j && number[i] === number[j]) {
+            console.log(`[${i}] = ${number[i]} == [${j}] = ${number[j]}`);
+            alert(`[${i}] = ${number[i]} так же как и [${j}] = ${number[j]}`);
+        }
+    }
+}
