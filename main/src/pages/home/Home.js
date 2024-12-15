36b4d5758ee089aaa3669a452543ab9444216892
Na-oni <miahoriacheva@gmail.com>
2024-12-15 09:41:44 +0200

    push

--- main/src/pages/home/Home.js
+++ main/src/pages/home/Home.js
@@ -1,11 +1,21 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './counter';
+
 import './Home.css';
 
-import TodoList from "./TodoList";
-
 function Home() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
     return (
         <div className="main-container">
-            <TodoList/>
+            <p className='counter'>Counter: {count}</p>
+
+            <div className='counter-buttons'>
+                <button onClick={() => dispatch(increment())}>+1</button>
+                <button onClick={() => dispatch(decrement())}>-1</button>
+                <button onClick={() => dispatch(reset())}>Reset</button>
+            </div>
         </div>
     );
 }
